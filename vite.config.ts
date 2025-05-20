d9f81d4b66ca53d87a9496e7baa0f0c87d5b04d4
Pranav Sathyan <106577296+PRANAVSATHYAN@users.noreply.github.com>
2025-05-21 00:49:05 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio-data-vision/", // ✅ IMPORTANT for GitHub Pages
+  base: "/portfolio-data-vision/", // 
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
