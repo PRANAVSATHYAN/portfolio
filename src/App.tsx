8d375db39e60ca8ee774eff98354216dbb4213c6
Pranav Sathyan <106577296+PRANAVSATHYAN@users.noreply.github.com>
2025-05-21 00:36:50 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <Sonner />
       <HashRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/portfolio-data-vision/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
